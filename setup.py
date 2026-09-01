8d7a23451476b1f806c98f16fc30c2361977e55a
nagix123-e <nagix@nifty.com>
2026-09-01 21:25:06 +0900

    Package OLCR v0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="olcr",
-    version="0.1.4",
+    version="0.1.5",
     description="Ollama Local Cognitive Runtime terminal client",
     license="Apache-2.0",
     python_requires=">=3.9",
